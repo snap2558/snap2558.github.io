5c65e8be7d5565ea763c2765e96ae62c25faeddf
snap2558 <47370013+snap2558@users.noreply.github.com>
2019-03-15 08:34:00 -0500

    Add files via upload

---
+++ Personal website/sketch.js
@@ -0,0 +1,7 @@
+var bio = {
+  "name": "Kyzr Snapko",
+  "info": "I am Kyzr Snapko, a computer science major at the University of St.Thomas who will graduate in the spring on 2020. I am active in the UST Computer Science club and have considerable knowledge in Java and C. So far I have completed the majority of the course load for the CISC program and am now searching for internship opportunities."
+}
+
+var output = document.getElementById('output');
+output.innerHTML = bio.info;
